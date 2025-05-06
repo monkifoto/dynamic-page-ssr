f7ef63222cf52360eee00b7c6833adaa77af1de6
sorin.bucse <sorin.bucse@gmail.com>
2025-05-06 17:10:12 +0000

    updated the server.ts to map the correct business id

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine, isMainModule } from '@angular/ssr/node';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import bootstrap from './main.server';
-
+import { SSR_BUSINESS_ID } from './app/tokens/server-request.token';
+import { appConfig } from './app/app.config';
+import { ApplicationConfig } from '@angular/core';
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 const indexHtml = join(serverDistFolder, 'index.server.html');
@@ -38,16 +40,39 @@ app.get(
 /**
  * Handle all other requests by rendering the Angular application.
  */
-app.get('**', (req, res, next) => {
+app.get('**', (req: Request, res: Response, next: NextFunction) => {
   const { protocol, originalUrl, baseUrl, headers } = req;
+
+  const functionTarget = (process.env['FUNCTION_TARGET'] || '').toLowerCase();
+  console.log('🏢 Server-side FUNCTION_TARGET:', functionTarget);
+
+  const functionToBusinessIdMap: { [key: string]: string } = {
+    "ssrhelpinghandafhcomtest": "vfCMoPjAu2ROVBbKvk0D",
+    "ssrhelpinghandafhcomprod": "vfCMoPjAu2ROVBbKvk0D",
+
+    "ssraefamilyhomecomtest": "UiSDf9elSjwcbQs2HZb1",
+    "ssraefamilyhomecomprod": "UiSDf9elSjwcbQs2HZb1",
+
+    "ssrelderlyhomecareafhcomtest": "SJgFxBYkopnPR4WibCAf",
+    "ssrelderlyhomecareafhcomprod": "SJgFxBYkopnPR4WibCAf",
+
+    "ssrprestigecareafhcomtest": "pDJgpl34XUnRblyIlBA7",
+    "ssrprestigecareafhcomprod": "pDJgpl34XUnRblyIlBA7",
+
+    "ssrcountrycrestafhcomtest": "yrNc50SvfPqwTSkvvygA",
+    "ssrcountrycrestafhcomprod": "yrNc50SvfPqwTSkvvygA",
+
+    "ssrsbmediahubcomtest": "MGou3rzTVIbP77OLmZa7",
+    "ssrsbmediahubcomprod": "MGou3rzTVIbP77OLmZa7",
+    "ssrserenityparkcomtest": "It4V1NeoAXQhXLJyQsf9",
+    "ssrserenityparkcomprod": "It4V1NeoAXQhXLJyQsf9",
+  };
+  const matchedKey = Object.keys(functionToBusinessIdMap).find(key => key.startsWith(functionTarget));
+  const businessId = matchedKey ? functionToBusinessIdMap[matchedKey] : 'MGou3rzTVIbP77OLmZa7';
 
   commonEngine
     .render({
-      bootstrap,
-      documentFilePath: indexHtml,
-      url: `${protocol}://${headers.host}${originalUrl}`,
-      publicPath: browserDistFolder,
-      providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+      bootstrap, documentFilePath: indexHtml, url: `${protocol}://${headers.host}${originalUrl}`, publicPath: browserDistFolder, providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }, { provide: SSR_BUSINESS_ID, useValue: businessId }]
     })
     .then((html) => res.send(html))
     .catch((err) => next(err));
